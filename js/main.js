022498e62a255a49350ea417634f4f60f0ee8781
CrafterSama <jolivero.03@gmail.com>
2016-06-23 18:58:32 -0400

    Fixed the Fixed option

--- js/main.js
+++ js/main.js
@@ -12,24 +12,26 @@ $(function() {
 		}
 	});
 });
-$(document).ready(function(){
 
-	$("#back-top").hide();
-
-	$(function () {
-		$(window).scroll(function () {
-			if ($(this).scrollTop() > 100) {
-				$('#back-top').fadeIn();
-			} else {
-				$('#back-top').fadeOut();
-			}
-		});
-
-		$('#back-top a').click(function () {
-			$('body,html').animate({
-				scrollTop: 0
-			}, 800);
-			return false;
-		});
-	}); 
+$(document).ready(function() {
+ 
+	var offset = 250;
+	 
+	var duration = 300;
+	 
+	$(window).scroll(function() {
+		if ($(this).scrollTop() > offset) {
+			$('.back-to-top').fadeIn(duration);
+		} else {
+			$('.back-to-top').fadeOut(duration); 
+		}
+	});
+	 
+	$('.back-to-top').click(function(event) {
+		event.preventDefault();
+		$('html, body').animate({ scrollTop: 0 }, duration);
+	 	return false;
+	});
+ 
 });
+ 
